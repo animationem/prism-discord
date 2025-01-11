791f8b8a3f3262c844236a6f735d6afc57551991
animationem <john.d.kesig@gmail.com>
2025-01-11 13:07:49 -0700

    added server option to settings menu

--- Scripts/util/settings_ui.py
+++ Scripts/util/settings_ui.py
@@ -30,6 +30,8 @@ class SettingsUI:
             lo_discord = QVBoxLayout(origin.w_discordProjectTab)
 
             self.createDiscordTokenSettingsMenu(lo_discord, origin)
+            self.createNotificationSettingsMenu(lo_discord, origin)
+            self.createServerSettingsMenu(lo_discord, origin)
 
             origin.addTab(origin.w_discordProjectTab, "Discord")
 
@@ -38,7 +40,7 @@ class SettingsUI:
         l_discord_logo = self.grabDiscordLogo()
 
         le_discord_token = QLineEdit()
-        le_discord_token.setPlaceholderText("Enter your Discord API Token")
+        le_discord_token.setPlaceholderText("Enter your Discord Bot Token")
         le_discord_token.setEchoMode(QLineEdit.Password)
         le_discord_token.setReadOnly(True)
         le_discord_token.setFocusPolicy(Qt.NoFocus)
@@ -58,6 +60,99 @@ class SettingsUI:
         lo_discord.addWidget(origin.b_discord_token)
         lo_discord.setAlignment(origin.b_discord_token, Qt.AlignBottom | Qt.AlignCenter)
 
+    @err_catcher(__name__)
+    def createNotificationSettingsMenu(self, lo_discord, origin):
+        gb_notifications = QGroupBox()
+        gb_notifications.setTitle("Notifications")
+        gb_notifications.setContentsMargins(0, 30, 0, 0)
+        lo_notifications = QVBoxLayout()
+        gb_notifications.setLayout(lo_notifications)
+
+        lo_notify_method = QHBoxLayout()
+        l_notify_method = QLabel("Notification Method:")
+        cb_notify_method = QComboBox()
+        origin.cb_notify_method = cb_notify_method
+
+        l_notify_method_help = self.grabHelpIcon()
+        l_notify_method_help.setToolTip("""<p style='line-height:1;'>
+                                        <span style='color:DodgerBlue;'><b>Direct</b></span>: Notify the selected user by Direct message<br>
+                                        <br>
+                                        <span style='color:Tomato;'><b>Channel</b></span>: Notify selected user in the Discord Channel<br>
+                                        <br>
+                                        <span style='color:MediumSeaGreen;'><b>Ephemeral Direct</b></span>: Notify the selected user in an ephemeral Direct message<br>
+                                        <br>
+                                        <span style='color:MediumSlateBlue;'><b>Ephemeral Channel</b></span>: Notify selected user in an ephemeral Channel message<br>
+                                        </p>""")
+
+        lo_notify_method.addWidget(l_notify_method)
+        lo_notify_method.addWidget(origin.cb_notify_method)
+        lo_notify_method.addWidget(l_notify_method_help)
+        lo_notify_method.addStretch()
+        lo_notifications.addLayout(lo_notify_method)
+
+        lo_discord.addWidget(gb_notifications)
+        lo_discord.setAlignment(gb_notifications, Qt.AlignTop)
+
+    @err_catcher(__name__)
+    def createServerSettingsMenu(self, lo_discord, origin):
+        gb_server = QGroupBox()
+        gb_server.setTitle("Server")
+        gb_server.setContentsMargins(0, 30, 0, 0)
+        lo_server = QVBoxLayout()
+        gb_server.setLayout(lo_server)
+
+        lo_status = QHBoxLayout()
+        l_server_status = QLabel("Status:")
+        l_server_status_value = QLabel("Offline")
+        fo_server_status_value = l_server_status_value.font()
+        fo_server_status_value.setItalic(True)
+        l_server_status_value.setFont(fo_server_status_value)
+        origin.l_server_status_value = l_server_status_value
+
+        lo_status.addWidget(l_server_status)
+        lo_status.addWidget(origin.l_server_status_value)
+        lo_status.addStretch()
+
+        b_server_button = QPushButton("Start Server")
+        origin.b_server_button = b_server_button
+        lo_status.addWidget(origin.b_server_button)
+
+        b_reset_button = QPushButton("Reset Server Status")
+        origin.b_reset_button = b_reset_button
+        lo_status.addWidget(origin.b_reset_button)
+
+        lo_machine = QHBoxLayout()
+        l_machine = QLabel("Machine:")
+        l_machine_value = QLabel()
+
+        lo_machine.addWidget(l_machine)
+        lo_machine.addWidget(l_machine_value)
+
+        lo_discord_server_name = QHBoxLayout()
+        le_discord_server = QLineEdit()
+        le_discord_server.setPlaceholderText("Enter your Discord Server Name")
+        l_discord_server_help = self.grabHelpIcon()
+        l_discord_server_help.setToolTip("""<p style='line-height:1;'>
+                                Enter your Discord Server Name<br><br>
+                                
+                                Note: Because the app can theoretically be installed on multiple servers, you need to specify which server you are trying to connect to.
+                                </p>""")
+        lo_discord_server_name.addWidget(le_discord_server)
+        lo_discord_server_name.addWidget(l_discord_server_help)
+
+        lo_save_server = QHBoxLayout()
+        lo_save_server.addStretch()
+        b_save_server_name = QPushButton("Save")
+        lo_save_server.addWidget(b_save_server_name)
+        lo_save_server.addStretch()
+
+        lo_server.addLayout(lo_status)
+        lo_server.addLayout(lo_machine)
+        lo_server.addLayout(lo_discord_server_name)
+        lo_server.addLayout(lo_save_server)
+
+        lo_discord.addWidget(gb_server)
+        lo_discord.setAlignment(gb_server, Qt.AlignTop)
         lo_discord.addStretch()
 
     @err_catcher(__name__)
@@ -76,3 +171,16 @@ class SettingsUI:
         l_discord.setContentsMargins(0, 0, 0, 0)
 
         return l_discord
+
+    @err_catcher(__name__)
+    def grabHelpIcon(self):
+        l_help = QLabel()
+        help_icon = os.path.join(
+            self.core.prismLibs, "Scripts", "UserInterfacesPrism", "help.png"
+        )
+
+        pixmap = QPixmap(help_icon)
+
+        l_help.setPixmap(pixmap)
+
+        return l_help
